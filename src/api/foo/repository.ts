47a11cbf075a28c21f8707b00705077433fbed5b
Diego Hideky <diegohideky@gmail.com>
2020-04-03 11:16:51 -0300

    Fixed husky version

--- src/api/foo/repository.ts
+++ src/api/foo/repository.ts
@@ -7,10 +7,9 @@ const findById = async (id: number): Promise<Foo> => Model.findByPk(id)
 
 const save = async (foo: Foo): Promise<Foo> => Model.create(foo)
 
-const updateById = async (
-  id: number,
-  foo: Foo,
-): Promise<Foo> => Model.update(foo, { where: { id } })
+const updateById = async (id: number, foo: Foo): Promise<Foo> => (
+  Model.update(foo, { where: { id } })
+)
 
 const removeById = async (id: number): Promise<Foo> => Model.destroy({ where: { id } })
 
